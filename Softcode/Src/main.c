5d5eb66cab4f06362b628dd1fb7b1e2e76195255
ejuoy <84949657@qq.com>
2020-09-03 12:06:14 +0800

    add CP010-221

---
+++ Softcode/Src/main.c
@@ -0,0 +1,22 @@
+/************************************************************************************************************/
+/*  File Function: MS51 DEMO project                                                                        */
+/************************************************************************************************************/
+
+#include "MS51_32K.h"
+
+
+void main (void) 
+{
+/* UART0 initial setting
+  * include sys.c in Library for modify HIRC value to 24MHz
+  * include uart.c in Library for UART initial setting
+*/
+    MODIFY_HIRC(HIRC_24);
+    P06_PUSHPULL_MODE;
+    UART_Open(24000000,UART0_Timer3,115200);
+    ENABLE_UART0_PRINTF;
+
+    printf("\n Hello world!");
+    while(1);
+
+}
--- Softcode/readme.txt
+++ Softcode/readme.txt
@@ -1,1 +1,6 @@
 2020.7.30 create soft prj
+
+Library 	:	MS51 lib
+Out	:	hex file
+Prj	:	keil5 prj 
+Src	:	all *.c *.h
